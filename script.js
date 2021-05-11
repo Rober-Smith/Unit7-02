426ac851c5fc91983c6322cb44d8316a678ea05e
RoberSmith <>
2021-05-11 23:45:30 +0000

    pls work

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-document.getElementById('button').addEventListener('click',compare)
 let userinput = 0
+
+document.getElementById('button').addEventListener('click', compare)
 
 function compare() {
   userInput = document.getElementById('input').value
   userInput =  parseInt(userInput)
 
   if (userInput < 0) {
-  userInput = document.getElementById('answer').innerHTML = "No, negetive"
+    userInput = document.getElementById('answer').innerHTML = 
+    "No, negetive"
   } else {
-  userInput = document.getElementById('answer').innerHTML = "Yes, positive"
+    userInput = document.getElementById('answer').innerHTML = 
+    "Yes, positive"
   }
 }
